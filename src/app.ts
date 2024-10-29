652a567e44255840ce9c04019b1683d74c53949f
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-10-29 22:07:47 +0500

    commit

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,9 @@ dotenv.config({ path: "./.env" });
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
 const port = Number(process.env.PORT) || 3000;
-import {schema} from "./graphql/schema/schema.js"
+import { schema } from "./graphql/schema/schema.js";
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers: { Query: { work: () => "hello World" } },
